0f3e53a7c4e118f5dd97e215e38d430722683d07
Leonardo <leosul@gmail.com>
2020-04-16 13:31:48 +0100

    Fix icons and header

--- client/src/pages/app/App.jsx
+++ client/src/pages/app/App.jsx
@@ -5,29 +5,34 @@ import { ToastContainer } from './../components'
 import './App.css'
 
 const App = ({ children }) => {
-  return (
-    <main className={'unauthorized'}>
-      <header className='app-header'>
-        <h1 className='app-title'>
-          <Link to='/'>
-            petmain
+    return (
+        <main>
+            <header className='app-header'>
+                <h1 className='app-title'>
+                    <Link to='/'>
+                        PetMain
             <span>beta</span>
-          </Link>
-        </h1>
-          <span className='user'>User Test</span>
-      </header>
-      <section className='content'>
-        {children}
-      </section>
-      <footer />
-      <ToastContainer />
-    </main>
-  )
+                    </Link>
+                </h1>
+                <span className='user'>
+                    <Link className='wrapper' to='/pets'>
+                        <span className='picture' />
+                        <span className='name'>User Test</span>
+                    </Link>
+                </span>
+            </header>
+            <section className='content'>
+                {children}
+            </section>
+            <footer />
+            <ToastContainer />
+        </main>
+    )
 }
 
 App.propTypes = {
-  children: PropTypes.object.isRequired,
-  user: PropTypes.object
+    children: PropTypes.object.isRequired,
+    user: PropTypes.object
 }
 
 export default App
