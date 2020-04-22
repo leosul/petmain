ff4bb0b9127eea0bb9922d8340710808646ae255
Leonardo <leosul@gmail.com>
2020-04-22 12:51:51 +0100

    Created loop map to inject components in this component

--- client/src/pages/components/tabControl/TabControl.jsx
+++ client/src/pages/components/tabControl/TabControl.jsx
@@ -1,27 +1,58 @@
 import React from 'react'
-//import { Link } from 'react-router-dom'
-//import PropTypes from 'prop-types'
-//import './StretchyButton.css'
-
-import AppBar from '@material-ui/core/AppBar';
+import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-//import Typography from '@material-ui/core/Typography';
-//import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-const TabControl = ({ icon, onChange, list, index }) => (
-    <AppBar position="static" color="default">
-        <Tabs
-            value={index}
-            indicatorColor="primary"
-            textColor="primary"
-            aria-label="scrollable force tabs example"
-            onChange={onChange}
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <Typography
+            component="div"
+            role="tabpanel"
+            hidden={value !== index}
+            id={`wrapped-tabpanel-${index}`}
+            aria-labelledby={`wrapped-tab-${index}`}
+            {...other}
         >
-            {list.map(l => <Tab label={l.name} icon={icon}/>)}
+            {value === index && <Box p={3}>{children}</Box>}
+        </Typography>
+    );
+}
 
-        </Tabs>
-    </AppBar>
+const TabControl = ({ store, onChange, index }) => (
+    <div>
+        <Paper>
+            <Tabs
+                value={index}
+                onChange={onChange}
+                indicatorColor='primary'
+                textColor='primary'
+                variant='fullWidth'
+                aria-label='full width tabs example'
+                centered
+            >
+                {store.map(s => {
+                    return (
+                        <Tab key={s.index} label={s.label} value={s.index} icon={s.icon} />
+                    )
+                })}
+            </Tabs>
+        </Paper>
+        <TabPanel value={index} index={index}>
+            <Paper>
+                {store.map(s => {
+                    if (s.index === index)
+                        return <h1>{s.label}</h1>
+                    else
+                        return null
+                }
+                )}
+            </ Paper >
+        </TabPanel>
+    </div>
 )
 
 export default TabControl
