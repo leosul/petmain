1d7af475f5ec4aecd98e4bcb05f88153514b1cfc
leosul@gmail.com <leosul@gmail.com>
2020-04-30 12:00:32 +0100

    Registered two new reducers LOGIN LOGOUT

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,30 @@
-import { SHOW_TOAST } from "./../actions";
+import { SHOW_TOAST, LOGIN, LOGOUT } from "./../actions";
+import { REHYDRATE } from "redux-persist";
 
 const initialState = {
     showToast: 0,
     toastMessage: "",
+    user: null,
 };
 
 function reducer(state = initialState, action) {
     switch (action.type) {
+        case LOGIN:
+            return {
+                ...state,
+                user: action.user,
+            };
+
+        case LOGOUT:
+            return {
+                ...state,
+                user: null,
+            };
+
+        case REHYDRATE:
+            if (action.payload) return {...state };
+            else return state;
+
         case SHOW_TOAST:
             return {
                 ...state,
