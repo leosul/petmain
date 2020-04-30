456b18a8ec117760e6c654805768fdc7929c664c
leosul@gmail.com <leosul@gmail.com>
2020-04-30 12:06:56 +0100

    Changed AuthorizedRoute to redirect depending on user

--- client/src/pages/authorizedRoute/AuthorizedRoute.jsx
+++ client/src/pages/authorizedRoute/AuthorizedRoute.jsx
@@ -1,29 +1,32 @@
 import React, { Component } from 'react'
-//import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Route } from 'react-router-dom'
+import { Redirect, Route } from 'react-router-dom'
 import Fetcher from './../../utils/Fetcher'
 
 class AuthorizedRoute extends Component {
-  render () {
-    const { component: Component, dispatch, ...rest } = this.props
-    return (
-      <Route {...rest} render={props => {
-        
-        props.fetcher = new Fetcher()
-        return <Component {...props} />
-      }} />
-    )
-  }
+    render() {
+        const { component: Component, user, dispatch, ...rest } = this.props
+        return (
+            <Route {...rest} render={props => {
+                if (!user) {
+                    return <Redirect to='/signin' />
+                }
+
+                props.user = user
+                props.fetcher = new Fetcher(user.token, dispatch)
+                return <Component {...props} />
+            }} />
+        )
+    }
 }
 
-//AuthorizedRoute.propTypes = {
-//  R: PropTypes.object.isRequired,
-//  user: PropTypes.object
-//}
+AuthorizedRoute.propTypes = {
+    user: PropTypes.object
+}
 
-const stateToProps = () => ({
-
+const stateToProps = ({ user }) => ({
+    user
 })
 
 export default connect(stateToProps)(AuthorizedRoute)
