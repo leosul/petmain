d0857607b935ff9c565fafe49f6887f15b377783
leosul@gmail.com <leosul@gmail.com>
2020-04-30 17:40:59 +0100

    Adjusted connection db

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose')
 const connectionString = process.env.MONGODB_URI
 
-//mongodb://admin:)0Custom@ds219839.mlab.com:19839/heroku_72hpp9c8
+//mongodb://localhost:27017/petmain
 
 const connect = () => {
-  return mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+    return mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
 }
 
 module.exports = { connect, mongoose }
-
